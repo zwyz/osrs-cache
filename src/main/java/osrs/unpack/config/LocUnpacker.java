aa777e9ccbf50d02b534003b1ffbdc462fd0ce63
zwyz <83980260+zwyz@users.noreply.github.com>
2025-10-20 17:02:19 -0400

    update to 235 (second staging client)

--- src/main/java/osrs/unpack/config/LocUnpacker.java
+++ src/main/java/osrs/unpack/config/LocUnpacker.java
@@ -206,6 +206,13 @@ public class LocUnpacker {
             case 93 -> lines.add("bgsoundfade=" + packet.g1() + "," + packet.g2() + "," + packet.g1() + "," + packet.g2()); // easeintype,easeinduration,easeouttype,easeoutduration
             case 94 -> lines.add("unknown94=no");
 
+            case 95 -> lines.add("crossworldsound=" + switch (packet.g1()) {
+                case 0 -> "yes";
+                case 1 -> "no";
+                case 2 -> "default"; // root world -> world entity only
+                default -> throw new IllegalStateException("unknown mode");
+            });
+
             case 249 -> {
                 var count = packet.g1();
 
